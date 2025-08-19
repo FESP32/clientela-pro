f3fe8e0ec62f2ea9f57c1bfff58604e45951e0d0
Fernando Sanchez <fesp321@gmail.com>
2025-08-19 00:27:15 -0600

    Updated config file

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,7 @@ const config = {
   appName: "Clientela Pro",
   appDescription:
     "Clientela Pro is a platform for businesses to manage their customer relationships and interactions effectively.",
-  domainName:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : process.env.NEXT_PUBLIC_BASE_URL,
+  domainName: process.env.NEXT_PUBLIC_BASE_URL
 };
 
 export default config;
